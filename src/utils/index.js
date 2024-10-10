2a51cc5320dc2028c5b45600e815c2137f67d011
smrsan <57676740+smrsan@users.noreply.github.com>
2024-10-10 17:28:23 +0330

    Export assets from src/utils

---
+++ src/utils/index.js
@@ -0,0 +1,28 @@
+export { default as heroImg } from "/assets/images/hero.jpeg";
+
+export { default as heroVideo } from "/assets/videos/hero.mp4";
+export { default as smallHeroVideo } from "/assets/videos/smallHero.mp4";
+export { default as highlightFirstVideo } from "/assets/videos/highlight-first.mp4";
+export { default as highlightSecondVideo } from "/assets/videos/hightlight-third.mp4";
+export { default as highlightThirdVideo } from "/assets/videos/hightlight-sec.mp4";
+export { default as highlightFourthVideo } from "/assets/videos/hightlight-fourth.mp4";
+export { default as exploreVideo } from "/assets/videos/explore.mp4";
+export { default as frameVideo } from "/assets/videos/frame.mp4";
+
+export { default as appleImg } from "/assets/images/apple.svg";
+export { default as searchImg } from "/assets/images/search.svg";
+export { default as bagImg } from "/assets/images/bag.svg";
+export { default as watchImg } from "/assets/images/watch.svg";
+export { default as rightImg } from "/assets/images/right.svg";
+export { default as replayImg } from "/assets/images/replay.svg";
+export { default as playImg } from "/assets/images/play.svg";
+export { default as pauseImg } from "/assets/images/pause.svg";
+
+export { default as yellowImg } from "/assets/images/yellow.jpg";
+export { default as blueImg } from "/assets/images/blue.jpg";
+export { default as whiteImg } from "/assets/images/white.jpg";
+export { default as blackImg } from "/assets/images/black.jpg";
+export { default as explore1Img } from "/assets/images/explore1.jpg";
+export { default as explore2Img } from "/assets/images/explore2.jpg";
+export { default as chipImg } from "/assets/images/chip.jpeg";
+export { default as frameImg } from "/assets/images/frame.png";
