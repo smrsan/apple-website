cab6921e2e98e464eb2f450a8bd92598befbe857
smrsan <57676740+smrsan@users.noreply.github.com>
2024-10-11 11:01:17 +0330

    Add a comment to the animations.js

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -35,7 +35,7 @@ export const animateWithGsapTimeline = (
             ...animationProps,
             ease: "power2.inOut",
         },
-        "<"
+        "<" // Insert the animation at the start of the previous animation
     );
 
     timeline.to(
